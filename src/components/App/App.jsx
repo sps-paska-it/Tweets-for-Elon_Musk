428668c501940b56763ffe6f32c3ca33b7d00126
sps-pashka-it <stetsyuk.p.s@gmail.com>
2023-06-10 01:30:22 +0300

    git deploy branch main

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { Layout } from '../Layout/Layout';
-import { Home } from 'pages/Home';
+import { Home } from 'pages/Home/Home';
 import { Tweets } from 'pages/Tweets/Tweets';
 
 export const App = () => {
